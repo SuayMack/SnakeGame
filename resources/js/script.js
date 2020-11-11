c8846f16322fb266bf3117e7968cabdcb05f9d1f
SuayMack <pry_chu@homail.com>
2020-11-11 19:18:52 -0300

    create game over

--- resources/js/script.js
+++ resources/js/script.js
@@ -69,9 +69,24 @@ function draw() {
     if(d == "RIGHT") snakeX += box;
     if(d == "DOWN") snakeY += box;
 
-    //remove the tail(calda)
-    snake.pop();
-    
+    //if the snake eats the food
+    if(snakeX == food.x && snakeY == food.y){
+        score++;
+        food = {
+            x: Math.floor(Math.random() * 17 + 1) * box,
+            y: Math.floor(Math.random() * 15 + 3) * box,
+        }
+        //we don't remove the tail
+    }else {
+        //remove the tail(calda)
+        snake.pop();
+    }
+
+    //game over
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box){
+        clearInterval(game);
+    }
+        
     // add new head
     let newHead = {
         x: snakeX,
