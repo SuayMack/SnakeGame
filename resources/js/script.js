6168a3ac869be3f41876275ba8cfb1194fb29db8
SuayMack <pry_chu@homail.com>
2020-11-11 18:49:34 -0300

    update Snake movement

--- resources/js/script.js
+++ resources/js/script.js
@@ -29,9 +29,27 @@ let food = {
 //create score
 let score = 0;
 
+//control the snake
+document.addEventListener("keydown", direction);
+
+function direction(event) {
+    let key = event.keyCode;
+    if(key == 37 && d != "RIGHT"){
+        d = "LEFT";
+    }else if(key == 38 && d != "DOWN"){
+        d = "UP";
+    }else if(key == 39 && d != "LEFT"){
+        d = "RIGHT";
+    }else if(key == 40 && d != "UP"){
+        d = "DOWN";
+    }
+}
+
 //draw everything to the canvas
 function draw() {
+
     ctx.drawImage(ground, 0, 0);
+
     for(let i = 0; i < snake.length; i++){
         ctx.fillStyle = (i == 0) ? "green" : "white";
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
@@ -40,6 +58,27 @@ function draw() {
         ctx.strokeRect(snake[i].x, snake[i].y, box, box);
     }
     ctx.drawImage(foodImg, food.x, food.y);
+
+    // old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    //which direction (qual direção)
+    if(d == "LEFT") snakeX -= box;
+    if(d == "UP") snakeY -= box;
+    if(d == "RIGHT") snakeX += box;
+    if(d == "DOWN") snakeY += box;
+
+    //remove the tail(calda)
+    snake.pop();
+    
+    // add new head
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
 
     ctx. fillStyle = "white";
     ctx.font = "45px Changa one";
