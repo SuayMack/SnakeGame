4d43327370b7080e04f6f05b8d1eb44caa3752e7
SuayMack <pry_chu@homail.com>
2020-11-12 00:18:29 -0300

    finished project

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,6 +16,21 @@ ground.src = "/resources/img/ground.png";
 const foodImg = new Image();
 foodImg.src = "/resources/img/food.png";
 
+// load audio files
+const dead = new Audio();
+const eat = new Audio();
+const up = new Audio();
+const right = new Audio();
+const left = new Audio();
+const down = new Audio();
+
+dead.src = "/resources/audio/dead.mp3";
+eat.src = "/resources/audio/eat.mp3";
+up.src = "/resources/audio/up.mp3";
+right.src = "/resources/audio/right.mp3";
+left.src = "/resources/audio/left.mp3";
+down.src = "/resources/audio/down.mp3";
+
 // create Snake
 let snake = [];
 snake[0] = {x: 9 * box, y: 10 * box};// iniciar a Snake em um lugar definido
@@ -30,19 +45,35 @@ let food = {
 let score = 0;
 
 //control the snake
+let d;
+
 document.addEventListener("keydown", direction);
 
 function direction(event) {
     let key = event.keyCode;
     if(key == 37 && d != "RIGHT"){
+        left.play();
         d = "LEFT";
     }else if(key == 38 && d != "DOWN"){
+        up.play();
         d = "UP";
     }else if(key == 39 && d != "LEFT"){
+        right.play();
         d = "RIGHT";
     }else if(key == 40 && d != "UP"){
+        down.play();
         d = "DOWN";
     }
+}
+
+//check collision function
+function collision(head, array) {
+    for (let i = 0; i < array.length; i++) {
+        if(head.x == array[i]. x && head.y == array[i].y){
+            return true;
+        }        
+    }
+    return false;
 }
 
 //draw everything to the canvas
@@ -72,6 +103,7 @@ function draw() {
     //if the snake eats the food
     if(snakeX == food.x && snakeY == food.y){
         score++;
+        eat.play();
         food = {
             x: Math.floor(Math.random() * 17 + 1) * box,
             y: Math.floor(Math.random() * 15 + 3) * box,
@@ -81,11 +113,6 @@ function draw() {
         //remove the tail(calda)
         snake.pop();
     }
-
-    //game over
-    if(snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box){
-        clearInterval(game);
-    }
         
     // add new head
     let newHead = {
@@ -93,6 +120,12 @@ function draw() {
         y: snakeY
     }
 
+    //game over
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box || collision(newHead, snake)){
+        clearInterval(game);
+        dead.play();
+    }
+    
     snake.unshift(newHead);
 
     ctx. fillStyle = "white";
@@ -102,19 +135,3 @@ function draw() {
 
 // call draw function every 100ms
 let game = setInterval(draw, 100);
-/*
-// load audio files
-let dead = new Audio();
-let eat = new Audio();
-let up = new Audio();
-let right = new Audio();
-let left = new Audio();
-let down = new Audio();
-
-dead.src = "audio/dead.mp3";
-eat.src = "audio/eat.mp3";
-up.src = "audio/up.mp3";
-right.src = "audio/right.mp3";
-left.src = "audio/left.mp3";
-down.src = "audio/down.mp3";
-*/
